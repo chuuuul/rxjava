d27049b796132e7ff7213c9efee1b1f18fad7a17
kim chul <chuuuul@naver.com>
2020-05-10 05:08:24 +0900

    5주차 기능 :  액티비티로 전환 코드 작성

--- android/app/src/main/java/com/example/rx_in_android/MainActivity.kt
+++ android/app/src/main/java/com/example/rx_in_android/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.rx_in_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_debounce.setOnClickListener {
+            startActivity(Intent(this, DebounceActivity::class.java))
+        }
+        btn_throttle.setOnClickListener {
+            startActivity(Intent(this, ThrottleActivity::class.java))
+        }
+        btn_buffer.setOnClickListener {
+            startActivity(Intent(this, BufferActivity::class.java))
+        }
+        btn_combineLasted.setOnClickListener {
+            startActivity(Intent(this, CombineLastest::class.java))
+        }
+        btn_behaviorSubject.setOnClickListener {
+            startActivity(Intent(this, BehaviorSubjectActivity::class.java))
+        }
+
     }
+
+
 }
